7182a4f37c55494e71debbc942f865c9f44b3c65
Cosmin Tanislav <demonsingur@gmail.com>
2021-09-14 23:56:08 +0300

    techpack: display: sde: encoder: remove intf_frame_count reading
    Signed-off-by: Kazuki Hashimoto <kazukih@tuta.io>

--- techpack/display/msm/sde/sde_encoder_phys_cmd.c
+++ techpack/display/msm/sde/sde_encoder_phys_cmd.c
@@ -301,12 +301,6 @@ static void sde_encoder_phys_cmd_te_rd_ptr_irq(void *arg, int irq_idx)
 	spin_unlock_irqrestore(phys_enc->enc_spinlock, lock_flags);
 
 	sde_encoder_helper_get_pp_line_count(phys_enc->parent, info, WRITE);
-	SDE_EVT32_IRQ(DRMID(phys_enc->parent),
-		info[0].pp_idx, info[0].intf_idx,
-		info[0].wr_ptr_line_count, info[0].intf_frame_count,
-		info[1].pp_idx, info[1].intf_idx,
-		info[1].wr_ptr_line_count, info[1].intf_frame_count,
-		scheduler_status);
 
 	mode = &phys_enc->cached_mode;
 	if (!mode || info[0].wr_ptr_line_count == mode->vdisplay ||
@@ -367,10 +361,6 @@ static void sde_encoder_phys_cmd_wr_ptr_irq(void *arg, int irq_idx)
 	}
 
 	sde_encoder_helper_get_pp_line_count(phys_enc->parent, info, WRITE);
-	SDE_EVT32_IRQ(DRMID(phys_enc->parent),
-		ctl->idx - CTL_0, event,
-		info[0].pp_idx, info[0].intf_idx, info[0].wr_ptr_line_count,
-		info[1].pp_idx, info[1].intf_idx, info[1].wr_ptr_line_count);
 
 #ifdef CONFIG_PXLW_IRIS
 	if (iris_is_chip_supported()) {
