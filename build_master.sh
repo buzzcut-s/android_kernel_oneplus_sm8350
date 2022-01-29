d3d34d271a650b22ca656da8245e6c028b7b24d4
Piyush Kumar <piyushkumg@gmail.com>
2022-01-29 13:55:05 +0530

    build_master: Rename kernel to bz
    Signed-off-by: Piyush Kumar <piyushkumg@gmail.com>

--- build_master.sh
+++ build_master.sh
@@ -8,14 +8,14 @@ fi
 VERSION="$(cat version)-$(date +%F | sed s@-@@g)"
 
 if [ -e boot.img ] ; then
-	rm arter97-kernel-$VERSION.zip 2>/dev/null
-	cp boot.img arter97-kernel-$VERSION.img
+	rm bz-kernel-$VERSION.zip 2>/dev/null
+	cp boot.img bz-kernel-$VERSION.img
 
 	# Pack AnyKernel3
 	rm -rf kernelzip
 	mkdir kernelzip
 	echo "
-kernel.string=arter97 kernel $(cat version) @ xda-developers
+kernel.string=bz kernel $(cat version) @ xda-developers
 do.devicecheck=0
 do.modules=0
 do.cleanup=1
@@ -29,10 +29,10 @@ ramdisk_compression=gzip
 	cp arch/arm64/boot/dtbo.img kernelzip/
 	touch kernelzip/vendor_boot
 	cd kernelzip/
-	7z a -mx9 arter97-kernel-$VERSION-tmp.zip *
-	7z a -mx0 arter97-kernel-$VERSION-tmp.zip ../arch/arm64/boot/Image.gz
-	zipalign -v 4 arter97-kernel-$VERSION-tmp.zip ../arter97-kernel-$VERSION.zip
-	rm arter97-kernel-$VERSION-tmp.zip
+	7z a -mx9 bz-kernel-$VERSION-tmp.zip *
+	7z a -mx0 bz-kernel-$VERSION-tmp.zip ../arch/arm64/boot/Image.gz
+	zipalign -v 4 bz-kernel-$VERSION-tmp.zip ../bz-kernel-$VERSION.zip
+	rm bz-kernel-$VERSION-tmp.zip
 	cd ..
-	ls -al arter97-kernel-$VERSION.zip
+	ls -al bz-kernel-$VERSION.zip
 fi
