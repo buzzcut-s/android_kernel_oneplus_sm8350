d29993023ada691d16a14a08c9c75fd3fbcd9555
Kazuki Hashimoto <kazukih@tuta.io>
2022-04-24 00:00:44 +0900

    Revert "sched/fair: Improve the scheduler"
    This reverts commit f1627cb4d62670f3a53cae4e925f2dae5e8a8550.
    
    Signed-off-by: Kazuki Hashimoto <kazukih@tuta.io>

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -609,15 +609,6 @@ static struct ctl_table kern_table[] = {
 	},
 #endif
 	{
-		.procname	= "sched_force_lb_enable",
-		.data		= &sysctl_sched_force_lb_enable,
-		.maxlen		= sizeof(unsigned int),
-		.mode		= 0644,
-		.proc_handler	= proc_dointvec_minmax,
-		.extra1		= SYSCTL_ZERO,
-		.extra2		= SYSCTL_ONE,
-	},
-	{
 		.procname	= "sched_min_granularity_ns",
 		.data		= &sysctl_sched_min_granularity,
 		.maxlen		= sizeof(unsigned int),
