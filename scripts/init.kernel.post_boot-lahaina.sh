c8c9c4c7cf0dcdca000ed99c2825f828843ec28a
Piyush Kumar <piyushkumg@gmail.com>
2022-06-06 15:13:04 +0530

    post_boot: Set little cluster's hispeed freq equal to input_boost_freq
    Let's use 1305Mhz as a common frequency point, instead of switching
    back and forth between 1209Mhz for hor hispeed and 1305Mhz for
    cpu_boost.
    
    Signed-off-by: Piyush Kumar <piyushkumg@gmail.com>

--- scripts/init.kernel.post_boot-lahaina.sh
+++ scripts/init.kernel.post_boot-lahaina.sh
@@ -181,11 +181,7 @@ echo 0 > /proc/sys/kernel/sched_boost
 echo "schedutil" > /sys/devices/system/cpu/cpufreq/policy0/scaling_governor
 echo 0 > /sys/devices/system/cpu/cpufreq/policy0/schedutil/down_rate_limit_us
 echo 0 > /sys/devices/system/cpu/cpufreq/policy0/schedutil/up_rate_limit_us
-if [ $rev == "1.0" ]; then
-    echo 1190400 > /sys/devices/system/cpu/cpufreq/policy0/schedutil/hispeed_freq
-else
-    echo 1209600 > /sys/devices/system/cpu/cpufreq/policy0/schedutil/hispeed_freq
-fi
+echo 1305600 > /sys/devices/system/cpu/cpufreq/policy0/schedutil/hispeed_freq
 echo 691200 > /sys/devices/system/cpu/cpufreq/policy0/scaling_min_freq
 echo 1 > /sys/devices/system/cpu/cpufreq/policy0/schedutil/pl
 
