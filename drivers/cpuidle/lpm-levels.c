d9f757bc7a5d298eab48e109d5449efa4b881950
Piyush Kumar <piyushkumg@gmail.com>
2022-08-14 13:11:19 +0530

    Revert "cpuidle: lpm-levels: Remove reduntant write memory barriers"
    This reverts commit 39dbc59710059b359fccde24140a6bdd5a936767.
    
    Signed-off-by: Piyush Kumar <piyushkumg@gmail.com>

--- drivers/cpuidle/lpm-levels.c
+++ drivers/cpuidle/lpm-levels.c
@@ -242,6 +242,8 @@ static void disable_rimps_timer(struct lpm_cpu *cpu)
 	ctrl_val = readl_relaxed(cpu->rimps_tmr_base + TIMER_CTRL);
 	writel_relaxed(ctrl_val & ~(TIMER_CONTROL_EN),
 				cpu->rimps_tmr_base + TIMER_CTRL);
+	/* Ensure the write is complete before returning. */
+	wmb();
 	spin_unlock(&cpu->cpu_lock);
 
 }
@@ -275,6 +277,8 @@ static void program_rimps_timer(struct lpm_cpu *cpu)
 	writel_relaxed(next_event, cpu->rimps_tmr_base + TIMER_VAL);
 	writel_relaxed(ctrl_val | (TIMER_CONTROL_EN),
 				cpu->rimps_tmr_base + TIMER_CTRL);
+	/* Ensure the write is complete before returning. */
+	wmb();
 	spin_unlock(&cpu->cpu_lock);
 }
 
